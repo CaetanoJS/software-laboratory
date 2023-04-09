e24a2d2a47695d358fdb416f876634c5205e5ebb
caetanojs <caetanoj.s@gmail.com>
2023-04-09 01:37:54 -0300

    chore: added new validations to customerController

--- server/controller/customersController.js
+++ server/controller/customersController.js
@@ -1,26 +1,45 @@
+const CustomerModel = require('../model/customersModel')
+const MessageHandler = require('../util/messageResponseHandling')
 module.exports = class CustomersController {
     constructor(db) {
         this.db = db
+        this.customerModel = new CustomerModel(db)
     }
 
-    async getCustomerById (userId) {
-        
+    async getCustomerById (customerId) {
+        let response = await this.customerModel.getCustomerById(customerId)
+        .then( (res) => {
+            return new MessageHandler().sucessMessage("Customer found", res)
+        })
+        .catch( (error) => {
+            return new MessageHandler().errorMessage(error.message, error)
+        })
+
+        return response
     }
 
     async getCustomers () {
         
     }
 
-    async deleteCustomers (user) {
+    async deleteCustomers (customer) {
 
     }
 
-    async editCustomers (user) {
+    async editCustomers (customer) {
 
     }
 
-    async addCustomers (user) {
+    async addCustomers (customer) {
+        let response = await this.customerModel.addCustomers(customer)
+        .then( (res) => {
+            return new MessageHandler().sucessMessage("Customer added correctly", res)
+        })
+        .catch( (error) => {
+            return new MessageHandler().errorMessage(error.message, error)
+        })
 
+        return response
     }
 
 }
